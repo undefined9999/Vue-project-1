39494d34e4afb1f351d30e5df0de7d78aef617f6
Mrj555 <516303259@qq.com>
2018-01-29 23:15:18 +0800

    详情

--- my-vuejs-project/src/router/index.js
+++ my-vuejs-project/src/router/index.js
@@ -1,38 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
 import Detail from '@/components/Detail_page1'
 
-import HelloWorld from '@/components/HelloWorld'
-import Carousel from '@/components/Carousel'
-
-import List1 from '@/components/list_page1'
-import Header from '@/components/Header'
-
-
-
-
-import Detail from '@/components/Detail_page1'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-
 		{
 
       path: '/deatail1', 
       name: 'Detail',
       component: Detail
-  }
-
-   
-  	{
-
-      path: '/list1', 
-      name: 'List1',
-      component: List1
-
-		},
+  },
   ]
 }) 
